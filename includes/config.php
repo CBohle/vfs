e382c4b1308074ce52e60dd90b2fa4f1e82cca26
CBohle <carolina.bohle.v@gmail.com>
2025-06-12 17:08:24 -0400

    Cambio config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,19 @@
 
 <?php
-define('BASE_URL', 'http://localhost/vfs/public/');
-define('BASE_ADMIN_URL', 'http://localhost/vfs/public/admin/');
+// Previo a los cambios de rutas
+// define('BASE_URL', 'http://localhost/vfs/public/');
+// define('BASE_ADMIN_URL', 'http://localhost/vfs/public/admin/');
+?>
+
+<?php
+$esLocal = ($_SERVER['HTTP_HOST'] === 'localhost');
+
+if ($esLocal) {
+    define('BASE_URL', 'http://localhost/vfs/');
+} else {
+    define('BASE_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
+}
+
+define('BASE_ADMIN_URL', BASE_URL . 'admin/');
+
 ?>
